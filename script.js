62fda3f416977a3d5a346d24fb4baef7129f8c9b
Rqmba <ramblack45@gmail.com>
2026-05-19 14:18:20 +0200

    feat: connect play button to getComputerChoice logic

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-console.log("Hello World");
-const playButton = document.querySelector("#signButton");
+const playButton = document.querySelector("#playButton");
 const log = document.querySelector("#log");
 
-function getComputerChoise() {
-  // Code
+function getComputerChoice() {
+  const random = Math.random();
+
+  if (random <= 0.33) {
+    return "Rock";
+  } else if (random <= 0.66) {
+    return "Paper";
+  } else {
+    random <= 0.33;
+    return "Scissors";
+  }
 }
+
+playButton.addEventListener("click", () => {
+  console.log("Computer choose : " + getComputerChoice());
+});
