25f52e1d891dcea2264e047d5e7b6530ab028c09
Rqmba <ramblack45@gmail.com>
2026-06-01 09:59:27 +0200

    feat: implement scoring logic and game loop structure
    - playRound now returns 'win', 'lose' or 'draw'
    
    - humanScore and computerScore declared in global scope
    
    - playGame receives playRound result as parameter
    
    - score increments correctly with humanScore++ / computerScore++
    
    - for loop structure identified for 5 rounds logic

--- script.js
+++ script.js
@@ -42,20 +42,40 @@ function playRound(humanChoice, computerChoice) {
   console.log("Player: " + humanChoice + " & Computer: " + computerChoice);
   if (humanChoice === "Rock" && computerChoice === "Paper") {
     console.log("You lose");
+    return "loose";
   } else if (humanChoice === "Paper" && computerChoice === "Scissors") {
+    return "loose";
     console.log("You lose");
   } else if (humanChoice === "Scissors" && computerChoice === "Rock") {
+    return "loose";
     console.log("You lose");
   } else if (humanChoice === computerChoice) {
     console.log("Draw");
+    return "draw";
   } else {
     console.log("You win");
+    return "win";
+  }
+}
+
+function playGame(choice) {
+  if (choice === "win") {
+    console.log("Round : " + choice);
+    humanScore++;
+  } else if (choice === "loose") {
+    console.log("Round : " + choice);
+    computerScore++;
+  } else {
+    console.log("Draw");
   }
 }
 
 playButton.addEventListener("click", () => {
   const humanSelection = getHumanChoice();
   const computerSelection = getComputerChoice();
-
-  playRound(humanSelection, computerSelection);
+  playGame(playRound(humanSelection, computerSelection));
+  console.log(
+    "Your Score : " + humanScore,
+    "Computer Score : " + computerScore,
+  );
 });
